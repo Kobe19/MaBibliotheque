8dee473dc4a0fc364ceed80d5980889502d718f7
daniel <fguirol@gmail.com>
2023-06-19 16:16:34 +0200

    sign user services with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from "firebase/app"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCK1SBJN31T2mabDy5nm76C4c8k-ajzCVU",
+      authDomain: "learningangularhttpclientdemo.firebaseapp.com",
+      databaseURL: "https://learningangularhttpclientdemo-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "learningangularhttpclientdemo",
+      storageBucket: "learningangularhttpclientdemo.appspot.com",
+      messagingSenderId: "186557765993",
+      appId: "1:186557765993:web:ec6b29fa714887441a05f8",
+      measurementId: "G-MKFDHMVE2S"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
