7dd777a723434fd4c1c08c3528311666a9670efe
daniel <fguirol@gmail.com>
2023-06-19 16:16:34 +0200

    sign user services with firebase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/auth';
+import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,38 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  createNewUser(email: string, password: string){
+    return new Promise<void>(
+      (resolve, reject) => {
+        firebase.createUserWithEmailAndPassword(firebase.getAuth(), email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        )
+      }
+    )
+  }
+
+  signInUser(email: string, password: string){
+    return new Promise<void>(
+      (resolve, reject) => {
+        firebase.signInWithEmailAndPassword(firebase.getAuth(), email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        )
+      }
+    )
+  }
+
+  signOutUser() {
+    firebase.signOut(firebase.getAuth());
+  }
 }
